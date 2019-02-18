816267bed2351e9cac9dbad6ec7db8ef6f682391
ZJouba <zjmonkey.joubert1@gmail.com>
2019-02-18 13:26:50 +0200

    Changes

--- Hands-On Machine Learning with Scikit-Learn and TensorFlow - Geron/Chapter 3/Ex1.py
+++ Hands-On Machine Learning with Scikit-Learn and TensorFlow - Geron/Chapter 3/Ex1.py
@@ -1,5 +1,24 @@
-from sklearn.datasets import fetch_mldata
+import scipy.io as sio
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.rcsetup as rcsetup
+import numpy as np
 
-mnist = fetch_mldata('MNIST original')
+mnist = sio.loadmat(
+    r'C:\Users\Zack\scikit_learn_data\mldata\mnist-original.mat')
 
-print(mnist.data.shape)
+X, y = mnist["data"], mnist["label"]
+
+X = np.transpose(X)
+
+some_digit = X[36000]
+some_digit_image = some_digit.reshape(28, 28)
+
+plt.imshow(some_digit_image, cmap=matplotlib.cm.binary,
+           interpolation="nearest")
+
+plt.axis("off")
+plt.show
+plt.ion()
+
+# print(rcsetup.all_backends)
