db8e0fb84b5b188d249b34b0fae6a0219cb0faef
ZJouba <zjmonkey.joubert1@gmail.com>
2019-02-18 13:41:33 +0200

    changes

--- Hands-On Machine Learning with Scikit-Learn and TensorFlow - Geron/Chapter 3/Ex1.py
+++ Hands-On Machine Learning with Scikit-Learn and TensorFlow - Geron/Chapter 3/Ex1.py
@@ -11,14 +11,18 @@ X, y = mnist["data"], mnist["label"]
 
 X = np.transpose(X)
 
-some_digit = X[36000]
-some_digit_image = some_digit.reshape(28, 28)
+# some_digit = X[36000]
+# some_digit_image = some_digit.reshape(28, 28)
 
-plt.imshow(some_digit_image, cmap=matplotlib.cm.binary,
-           interpolation="nearest")
+# plt.imshow(some_digit_image, cmap=matplotlib.cm.binary,
+#            interpolation="nearest")
 
-plt.axis("off")
-plt.show
-plt.ion()
+# plt.axis("off")
+# plt.draw()
+# plt.show()
 
-# print(rcsetup.all_backends)
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+shuffle_index = np.random.permutation(60000)
+
+print(shuffle_index)
