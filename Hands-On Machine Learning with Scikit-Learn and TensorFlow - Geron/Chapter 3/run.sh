d92273aa395a94da6af2781c85fb98026d87de8f
ZJouba <zjmonkey.joubert1@gmail.com>
2019-02-20 16:31:30 +0200

    changes

--- Hands-On Machine Learning with Scikit-Learn and TensorFlow - Geron/Chapter 3/run.sh
+++ Hands-On Machine Learning with Scikit-Learn and TensorFlow - Geron/Chapter 3/run.sh
@@ -7,7 +7,15 @@
 #PBS -l walltime=1:00:00
 
 # Select cores
-#PBS -l select=1:ncpus=8
+#PBS -l select=1:ncpus=16:mem=16GB
+
+# Notify me
+#PBS -m be
+#PBS -M 22115536@sun.ac.za
+
+# Logs and outputs
+#PBS -e Ex1.err
+#PBS -o Ex1.out
 
 # Switch directories
 cd $PBS_O_WORKDIR
