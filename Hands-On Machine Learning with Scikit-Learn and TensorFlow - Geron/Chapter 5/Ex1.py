bdbbb42e2aff40ace2979b5b315fc79386201823
ZJouba <zjmonkey.joubert1@gmail.com>
2019-03-04 16:21:33 +0200

    Completed

--- Hands-On Machine Learning with Scikit-Learn and TensorFlow - Geron/Chapter 5/Ex1.py
+++ Hands-On Machine Learning with Scikit-Learn and TensorFlow - Geron/Chapter 5/Ex1.py
@@ -1,84 +1,65 @@
 import numpy as np
 
 from sklearn import datasets
-from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC, SVC
 from sklearn.linear_model import SGDClassifier
 import matplotlib.pyplot as plt
+from labellines import labelLines
 
 iris = datasets.load_iris()
-X = iris['data'][:, (2,3)]
+X = iris['data'][:, (2, 3)]
 y = (iris['target'] == 2).astype(np.float64)
 
-def make_meshgrid(x, y, h=0.02):
-    """Create a mesh of points to plot in
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
 
-    Parameters
-    ----------
-    x: data to base x-axis meshgrid on
-    y: data to base y-axis meshgrid on
-    h: stepsize for meshgrid, optional
 
-    Returns
-    -------
-    xx, yy : ndarray
-    """
-    x_min, x_max = x.min() - 1, x.max() + 1
-    y_min, y_max = y.min() - 1, y.max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                         np.arange(y_min, y_max, h))
-    return xx, yy
-
-def plot_contours(ax, clf, xx, yy, **params):
-    """Plot the decision boundaries for a classifier.
-
-    Parameters
-    ----------
-    ax: matplotlib axes object
-    clf: a classifier
-    xx: meshgrid ndarray
-    yy: meshgrid ndarray
-    params: dictionary of params to pass to contourf, optional
-    """
-    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
-    out = ax.contourf(xx, yy, Z, **params)
-    return out
-
-# we create an instance of SVM and fit out data. We do not scale our
-# data since we want to plot the support vectors
-C = 1.0  # SVM regularization parameter
+C = 1.0
 models = (
-	LinearSVC(C=1, loss='hinge', random_state=42),
-	SVC(kernel='linear', C=C),
-  SGDClassifier(loss='hinge', alpha=(1/(len(X)*C)),max_iter=1000000, tol=-np.infty, random_state=42)
+    LinearSVC(
+        C=C,
+        loss='hinge',
+        random_state=42,
+        max_iter=10000
+    ),
+    SVC(
+        kernel='linear',
+        C=C
+    ),
+    SGDClassifier(
+        loss='hinge',
+        alpha=(1/(len(X)*C)),
+        max_iter=10000,
+        tol=-np.infty,
+        random_state=42
+    )
 )
-models = (clf.fit(X, y) for clf in models)
 
-# title for the plots
-titles = ('LinearSVC',
-          'SVC',
-          'SGDClassifier'
-)
+models = (model.fit(X_scaled, y) for model in models)
 
-# Set-up 2x2 grid for plotting.
-fig, sub = plt.subplots(2, 2)
-plt.subplots_adjust(wspace=0.4, hspace=0.4)
+plt.figure()
 
-X0, X1 = X[:, 0], X[:, 1]
-xx, yy = make_meshgrid(X0, X1)
+meshX, meshY = X_scaled[:, 0], X_scaled[:, 1]
+x_min, x_max = (meshX.min() - 1, meshX.max() + 1)
+y_min, y_max = (meshY.min() - 1, meshY.max() + 1)
 
-for clf, title, ax in zip(models, titles, sub.flatten()):
-    plot_contours(ax, clf, xx, yy,
-                  cmap=plt.cm.coolwarm, alpha=0.8)
-    ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
-    ax.set_xlim(xx.min(), xx.max())
-    ax.set_ylim(yy.min(), yy.max())
-    ax.set_xlabel('Sepal length')
-    ax.set_ylabel('Sepal width')
-    ax.set_xticks(())
-    ax.set_yticks(())
-    ax.set_title(title)
+xx = np.linspace(x_min, x_max)
 
+styles = ('k--', 'b:', 'r-')
+
+for model, style in zip(models, styles):
+    a = -model.coef_[0, 0] / model.coef_[0, 1]
+    b = -model.intercept_[0] / model.coef_[0, 1]
+    name = type(model).__name__
+    plt.plot(xx, a * xx + b, style, alpha=0.5, label=name)
+    plt.scatter(meshX, meshY, c=y, cmap=plt.cm.coolwarm,
+                s=20, edgecolors='k')
+
+labelLines(plt.gca().get_lines(), zorder=2.5)
+plt.xlim(xx.min(), xx.max())
+plt.ylim(xx.min(), xx.max())
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+plt.title('Model comparison')
 plt.show()
