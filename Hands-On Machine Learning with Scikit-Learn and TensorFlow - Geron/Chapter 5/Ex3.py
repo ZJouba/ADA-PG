35b8ad8aa4e28b58184b459d010ce1fdd21d6c28
Joubert <22115536@sun.ac.za>
2019-03-05 11:44:14 +0200

    changes

--- Hands-On Machine Learning with Scikit-Learn and TensorFlow - Geron/Chapter 5/Ex3.py
+++ Hands-On Machine Learning with Scikit-Learn and TensorFlow - Geron/Chapter 5/Ex3.py
@@ -1,34 +1,30 @@
-import os
-import tarfile
-from six.moves import urllib
-import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import fetch_california_housing
+from sklearn.svm import SVR
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import reciprocal, uniform
+from sklearn.externals import joblib
 
-from sklearn.model_selection import StratifiedShuffleSplit
+housing = fetch_california_housing()
+X = housing["data"]
+y = housing["target"]
 
-DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
-HOUSING_PATH = "datasets/housing"
-HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + "/housing.tgz"
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=9)
 
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 
-def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
-    if not os.path.isdir(housing_path):
-    os.makedirs(housing_path)
-    tgz_path = os.path.join(housing_path, "housing.tgz")
-    urllib.request.urlretrieve(housing_url, tgz_path)
-    housing_tgz = tarfile.open(tgz_path)
-    housing_tgz.extractall(path=housing_path)
-    housing_tgz.close()
+params = {
+    "gamma": reciprocal(0.001, 0.1),
+    "C": uniform(0.1, 5)
+}
 
+randomS = RandomizedSearchCV(
+    SVR(), params, n_iter=50, verbose=1, cv=4, random_state=9)
 
-def load_housing_data(housing_path=HOUSING_PATH):
-    csv_path = os.path.join(housing_path, "housing.csv")
-    return pd.read_csv(csv_path)
+randomS.fit(X_train_scaled, y_train)
 
-
-housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
-housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
-
-split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-
-for train_index, test_index in split.split(housing, housing["income_cat"]):
-    strat_train_set = housing.loc[train_index]
+joblib.dump(randomS.best_estimator_, 'randomS_bestE.pkl', compress = 1)
