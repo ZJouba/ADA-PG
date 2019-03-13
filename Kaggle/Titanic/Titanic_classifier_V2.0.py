5c4a16fe2e1df1b1dd0952376d0736e51f7ec0ab
ZJouba <zjmonkey.joubert1@gmail.com>
2019-03-13 12:03:01 +0200

    changes

--- Kaggle/Titanic/Titanic_classifier_V2.0.py
+++ Kaggle/Titanic/Titanic_classifier_V2.0.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import pandas as pd
 import numpy as np
 import seaborn as sb
@@ -15,44 +16,53 @@ testData = pd.read_csv(testFile, header=0, dtype={'Age': np.float64})
 
 allData = [trainData, testData]
 
+if os.path.isfile(os.path.join(os.path.dirname(__file__), 'full.csv')):
+    if input('Data csv already exists. Redo? [y/n] \t') == 'y':
+        def titles(name):
+            search = re.search(' ([A-Za-z]+)\.', name)
+            if search:
+                return search.group(1)
+            return ""
 
-def titles(name):
-    search = re.search(' ([A-Za-z]+)\.', name)
-    if search:
-        return search.group(1)
-    return ""
 
+        commonTitles = {
+            'Mlle': 'Miss',
+            'Ms': 'Miss',
+            'Miss': 'Miss',
+            'Mme': 'Mrs',
+            'Mrs': 'Mrs',
+            'Master': 'Master',
+            'Mr': 'Mr'
+        }
 
-commonTitles = {
-    'Mlle': 'Miss',
-    'Ms': 'Miss',
-    'Miss': 'Miss',
-    'Mme': 'Mrs',
-    'Mrs': 'Mrs',
-    'Master': 'Master',
-    'Mr': 'Mr'
-}
+        for dataset in allData:
+            dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+            dataset['Alone'] = 0
+            dataset.loc[dataset['FamilySize'] == 1, 'Alone'] = 1
+            dataset['Level'] = dataset['Cabin'].str[0]
+            dataset['Title'] = dataset['Name'].apply(titles)
+            dataset['Title'] = dataset['Title'].map(commonTitles).fillna('Rare')
 
-for dataset in allData:
-    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
-    dataset['Alone'] = 0
-    dataset.loc[dataset['FamilySize'] == 1, 'Alone'] = 1
-    dataset['Level'] = dataset['Cabin'].str[0]
-    dataset['Title'] = dataset['Name'].apply(titles)
-    dataset['Title'] = dataset['Title'].map(commonTitles).fillna('Rare')
+        trainData['AgeGroups'] = pd.cut(trainData['Age'], 10)
 
-trainData['AgeGroups'] = pd.cut(trainData['Age'], 10)
+        grouped = trainData.groupby(['Sex', 'Pclass', 'Title'])
 
-grouped = trainData.groupby(['Sex', 'Pclass', 'Title'])
+        trainData['Age'] = grouped['Age'].apply(lambda x: x.fillna(x.median()))
+        trainData.loc[trainData['Fare'] == 0.0, 'Fare'] = np.NaN
+        trainData['Fare'] = grouped['Fare'].apply(lambda x: x.fillna(x.median()))
 
-trainData['Age'] = grouped['Age'].apply(lambda x: x.fillna(x.median()))
-trainData.loc[trainData['Fare'] == 0.0, 'Fare'] = np.NaN
-trainData['Fare'] = grouped['Fare'].apply(lambda x: x.fillna(x.median()))
+        embarkedMost = trainData['Embarked'].value_counts().index[0]
 
-# trainData.loc[trainData['Fare'] == 0.0, 'Fare'] = grouped['Fare'].apply(lambda x: x.median())
+        trainData['Embarked'] = trainData['Embarked'].fillna(embarkedMost)
 
-embarkedMost = trainData['Embarked'].value_counts().index[0]
+        trainData.to_csv(os.path.join(os.path.dirname(__file__), 'full.csv'), sep=',')
+    else:
 
-trainData['Embarked'].replace(r'^\s+$', embarkedMost, regex=True, inplace=True)
+        trainData = pd.read_csv(os.path.join(os.path.dirname(__file__), 'full.csv'), header=0, dtype={'Age': np.float64})
 
-trainData.to_csv(os.path.join(os.path.dirname(__file__), 'full.csv'), sep=',')
+cabinTrain = trainData.dropna(subset=['Cabin'])
+
+cabinTrain = trainData.drop(['Name', 'Ticket', 'AgeGroups'])
+
+cabinTrain.to_csv(os.path.join(os.path.dirname(
+    __file__), 'cabinTrain.csv'), sep=',')
