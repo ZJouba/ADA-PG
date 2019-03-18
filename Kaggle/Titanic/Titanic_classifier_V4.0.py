47d48f3cebe5ca7698a18fc4668e0017712ddaef
ZJouba <zjmonkey.joubert1@gmail.com>
2019-03-18 17:30:29 +0200

    changes

--- Kaggle/Titanic/Titanic_classifier_V4.0.py
+++ Kaggle/Titanic/Titanic_classifier_V4.0.py
@@ -207,7 +207,28 @@ def trainModel(finalData):
         tol=0.0001,
         warm_start=False
     )
-    D = MLPClassifier()
+    D = MLPClassifier(
+        activation='relu',
+        alpha=1e-05,
+        batch_size='auto',
+        beta_1=0.9,
+        beta_2=0.999,
+        early_stopping=False,
+        epsilon=1e-08,
+        hidden_layer_sizes=(50, 50),
+        learning_rate='constant',
+        learning_rate_init=0.001,
+        max_iter=200,
+        momentum=0.9,
+        nesterovs_momentum=True,
+        power_t=0.5,
+        random_state=1,
+        shuffle=True,
+        solver='lbfgs',
+        tol=0.0001,
+        validation_fraction=0.1,
+        warm_start=False
+    )
 
     finalModel = VotingClassifier(
         estimators=[('A', A), ('B', B), ('C', C), ('D', D)],
